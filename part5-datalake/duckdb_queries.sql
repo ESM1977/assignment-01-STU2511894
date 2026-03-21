af64aaa7287f85edb84e7309cc014a7affca222c
Esmita Gupta <esmita.gupta@bkbck.edu.in>
2026-03-21 17:34:30 +0530

    Update duckdb_queries.sql

--- part5-datalake/duckdb_queries.sql
+++ part5-datalake/duckdb_queries.sql
@@ -1,9 +1,6 @@
 -- Q1: List all customers along with the total number of orders they have placed
 
-SELECT 
-    c.customer_id,
-    c.name,   -- change to customer_name if needed
-    COUNT(o.order_id) AS total_orders
+SELECT c.customer_id, c.name, COUNT(o.order_id) AS total_orders
 FROM read_csv_auto('datasets/customers.csv') c
 LEFT JOIN read_json_auto('datasets/orders.json') o
     ON c.customer_id = o.customer_id
@@ -11,9 +8,7 @@ GROUP BY c.customer_id, c.name;
 
 -- Q2: Find the top 3 customers by total order value
 
-SELECT 
-    c.name,   -- change if needed
-    SUM(o.total_amount) AS total_value
+SELECT c.name, SUM(o.total_amount) AS total_value
 FROM read_csv_auto('datasets/customers.csv') c
 JOIN read_json_auto('datasets/orders.json') o
     ON c.customer_id = o.customer_id
@@ -22,8 +17,7 @@ ORDER BY total_value DESC
 LIMIT 3;
 
 -- Q3: List all products purchased by customers from Bangalore
-SELECT DISTINCT 
-    c.name
+SELECT DISTINCT c.name
 FROM read_csv_auto('datasets/customers.csv') c
 JOIN read_json_auto('datasets/orders.json') o
     ON c.customer_id = o.customer_id
@@ -31,11 +25,7 @@ WHERE c.city = 'Bangalore';
 
 -- Q4: Show customer name, order date, product name, quantity
 
-SELECT 
-    c.name,
-    o.order_date,
-    o.total_amount,
-    o.num_items
+SELECT c.name, o.order_date, o.total_amount,o.num_items
 FROM read_csv_auto('datasets/customers.csv') c
 JOIN read_json_auto('datasets/orders.json') o
     ON c.customer_id = o.customer_id;
